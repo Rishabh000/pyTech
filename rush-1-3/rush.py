9196ee7b745241374afd26f43b45a979028be20d
Rishabh Ranka <rishabhranka9324@gmail.com>
2026-05-11 22:52:57 -0400

    completed assignment 3

---
+++ rush-1-3/rush.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def rush(x, y):
+    if x <= 0 or y <= 0:
+        print("Invalid size", file=sys.stderr)
+        return
+
+    if x == 1 or y == 1:
+        for _ in range(y):
+            print("B" * x)
+        return
+
+    for row in range(y):
+        line = []
+        for col in range(x):
+            is_top = row == 0
+            is_bottom = row == y - 1
+            is_left = col == 0
+            is_right = col == x - 1
+
+            if is_top and (is_left or is_right):
+                line.append("A")
+            elif is_bottom and (is_left or is_right):
+                line.append("C")
+            elif is_top or is_bottom or is_left or is_right:
+                line.append("B")
+            else:
+                line.append(" ")
+        print("".join(line))
